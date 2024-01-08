3e0990397ea8403b6d333c886698286c715c3278
anniekjansen <71121209+anniekjansen@users.noreply.github.com>
2024-01-08 19:30:00 +0100

    Update DataAnalyzer and exploration

--- DataAnalyzer.py
+++ DataAnalyzer.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 class DataAnalyzer:
 
@@ -9,13 +10,41 @@ class DataAnalyzer:
             print(column)
 
     def print_info(self, data):
+        print(data.shape) # rows and columns
         print(data.head())
+        print(data.tail())
         print(data.iloc[0])
-        print(data.shape)
+        print(data.iloc[35683])
         print(data.dtypes)
+        print(data.nunique())
+        print(data.isnull().sum())
+        print("Total # of missing values:", data.isnull().sum().sum())
 
     def print_histogram(self, data, column_title):
         data[column_title].value_counts().plot(kind='bar')
+        plt.show()
+
+    def print_barchart_kans_schade(self, data, columns):
+        df1 = pd.DataFrame()
+        df2 = pd.DataFrame()
+        df1['Kans'] = data["Kans"]
+        df2['Schade'] = data["Schade"]
+
+        name_sort = {'Low':0,'Medium':1,'High':2}
+        df1['name_sort'] = df1['Kans'].map(name_sort)
+        df2['name_sort'] = df2['Schade'].map(name_sort)
+     
+        df1 = df1.sort_values(by=['name_sort'])
+        df2 = df2.sort_values(by=['name_sort'])
+
+        width = 0.25
+        df1['Kans'].value_counts(sort=False).plot(kind='bar', width = width, position=1, title="Bar Chart Kans & Schade")
+        df2['Schade'].value_counts(sort=False).plot(kind='bar', color='skyblue', position=0, width = width) 
+        plt.legend(["Kans", "Schade"])
+        plt.ylabel("Counts")
+        plt.xlabel("")
+        plt.xticks(rotation ='horizontal')
+        plt.savefig("./figures/BarChartKansSchade")
         plt.show()
 
     def print_selected_columns(self, data, columns): 
@@ -32,6 +61,7 @@ class DataAnalyzer:
         print('Total # of duplicates:', data.duplicated().sum())
 
     def check_duplicates_columns(self, data, columns):
+        print(data.duplicates())
         for col in columns:
             print(col, ':', data.duplicated(subset=[col]).sum(), 'duplicates')
 
--- exploration.py
+++ exploration.py
@@ -7,12 +7,14 @@ from DataAnalyzer import DataAnalyzer
 
 """ Load initial dataset """
 data = DataLoaderSaver().load_dataset("initial")
-print(data)
+# print(data)
 
 """ Exploring the dataset """
 # DataAnalyzer().print_column_names(data)
-# DataAnalyzer().print_info(data)
+DataAnalyzer().print_info(data)
 # DataAnalyzer().print_histogram(data, "Kans")
+# DataAnalyzer().print_histogram(data, "Schade")
+DataAnalyzer().print_barchart_kans_schade(data)
 # DataAnalyzer().print_selected_columns(data, ["Titel","NCSC ID","Advisory ID"])
 # DataAnalyzer().print_selected_cell(data, 1, "Schadeomschrijving")
 # DataAnalyzer().print_selected_value(data, "NCSC ID", 'NCSC-2023-0023')
@@ -21,10 +23,10 @@ print(data)
 
 """ Check for duplicates in the dataset """
 DataAnalyzer().check_duplicates(data)
-DataAnalyzer().check_duplicates_columns(data, ['Advisory ID', 'NCSC ID'])
+# DataAnalyzer().check_duplicates_columns(data, ['Advisory ID', 'NCSC ID'])
 
 """ Check for unique values in the dataset """
-# DataAnalyzer().check_uniques(data)
-DataAnalyzer().check_uniques_columns(data, ['Advisory ID', 'NCSC ID'])
+DataAnalyzer().check_uniques(data)
+# DataAnalyzer().check_uniques_columns(data, ['Advisory ID', 'NCSC ID'])
 
 
