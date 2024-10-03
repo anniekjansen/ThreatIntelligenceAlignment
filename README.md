60d1cd379ef862b65266df1882268eb1c9fc2894
anniekjansen <71121209+anniekjansen@users.noreply.github.com>
2024-10-03 16:15:26 +0200

    Little updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Code for Thesis
+# Repository for Thesis
 
 MSc Artificial Intelligence
 
--- RQ2_comp_analysis.py
+++ RQ2_comp_analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 from CompAnalyzer import CompAnalyzer
 
 """ 6.1 """
+print("6.1")
 product_df = pd.read_csv('./SPARQL Queries/Results/query-result-6.1.csv')
 justification_binary = product_df['justificationType'].apply(lambda x: 'Justified' if x == 'Justified' else 'Non-Justified')
 contingency_table = pd.pivot_table(product_df, index='productName', columns=justification_binary, aggfunc='size', fill_value=0)
@@ -11,6 +12,7 @@ chi2, p_chi2, dof = CompAnalyzer().perform_chi_squared_test(contingency_table)
 CompAnalyzer().print_results(odds_ratios, chi2, p_chi2, dof)
 
 """ 6.2 """
+print("6.2")
 df = pd.read_csv('./SPARQL Queries/Results/query-result-6.2.csv')
 likelihood_binary = df['likelihoodLabel'].apply(lambda x: 'High' if x == 'High' else 'Low/Medium')
 contingency_table = pd.crosstab(likelihood_binary, df['productName'])
@@ -20,6 +22,7 @@ odds_ratios = CompAnalyzer().calculate_odds_ratios(contingency_table_odds, 'High
 CompAnalyzer().print_results(odds_ratios, chi2, p_chi2, dof)
 
 """ 6.3 """
+print("6.3")
 likelihood_df = pd.read_csv('./SPARQL Queries/Results/query-result-6.3.csv')
 likelihood_binary = likelihood_df['likelihoodType'].apply(lambda x: 'High' if x == 'High' else 'Low/Medium')
 contingency_table = pd.crosstab(likelihood_binary, likelihood_df['justificationType'], values=likelihood_df['count'], aggfunc='sum')
